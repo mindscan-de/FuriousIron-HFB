217308b36584cc805bb85e6f1bd898d2c64c8826
mindscan-de <maxim.gansert@gmail.com>
2022-05-13 20:35:13 +0200

    add an idea, for the second hfbv2 filterbank writer.

---
+++ src/de/mindscan/furiousiron/hfb/io/HFBFilterBankWriterV2Impl.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ * MIT License
+ *
+ * Copyright (c) 2022 Maxim Gansert, Mindscan
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package de.mindscan.furiousiron.hfb.io;
+
+import de.mindscan.furiousiron.hfb.HFBFilterBank;
+import de.mindscan.furiousiron.hfb.HFBFilterBankWriter;
+
+/**
+ * 
+ * This is the second version of the HFBFilterbank Writer. This encodes the byte data, which is 
+ * actually used as a bitset using a golomb code. Because we use a load factor of at least three
+ * to five we expect more zeros bits in a single byte. that will allow to compress bytes using 
+ * fewer bits.
+ *  
+ */
+public class HFBFilterBankWriterV2Impl implements HFBFilterBankWriter {
+
+    /** 
+     * {@inheritDoc}
+     */
+    @Override
+    public void write( HFBFilterBank filterBank, String outputPath ) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
