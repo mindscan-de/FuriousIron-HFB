e3cafc5f2b5f45968103e25c80db0b9564808ec6
mindscan-de <maxim.gansert@gmail.com>
2022-05-15 12:12:32 +0200

    provide data generator (as test) to calculate ByteWeightMatrix

---
+++ test/de/mindscan/furiousiron/hfb/io/BitwiseCalculationsTest.java
@@ -0,0 +1,34 @@
+package de.mindscan.furiousiron.hfb.io;
+
+import org.junit.jupiter.api.Test;
+
+public class BitwiseCalculationsTest {
+
+    @Test
+    public void testGetByteWeights_calculateMatrix_expectCodeOuptut() throws Exception {
+        byte[] hexdigitweights = BitwiseCalculations.getHexDigitWeight();
+
+        for (int j = 0; j < 16; j++) {
+
+            StringBuffer commentLine = new StringBuffer();
+            commentLine.append( "//" );
+            for (int i = 0; i < 16; i++) {
+                commentLine.append( String.format( " 0x%x%x", j, i ) );
+            }
+
+            System.out.println( commentLine.toString() );
+
+            StringBuffer codeLine = new StringBuffer();
+            commentLine.append( "  " );
+
+            for (int i = 0; i < 16; i++) {
+                int byteweight = hexdigitweights[j] + hexdigitweights[i];
+                codeLine.append( String.format( "   %d,", byteweight ) );
+            }
+
+            codeLine.append( " //" );
+            System.out.println( codeLine.toString() );
+        }
+    }
+
+}
